ef600912c8032fb9fdb2e7e3bce5ac5132d5c12a
Phil Merricks <seffyroff@gmail.com>
2018-11-21 11:13:53 -0800

    initial commit local branch region

--- reactive/layer_maas_region_lxd.py
+++ reactive/layer_maas_region_lxd.py
@@ -1,7 +1,18 @@
 from subprocess import call
 
+from charms.reactive import (
+    when,
+    when_not,
+    when_any,
+    set_flag,
+    clear_flag,
+    endpoint_from_flag,
+    is_flag_set,
+    hook,
+    set_state,
+)
+
 from charmhelpers.core.hookenv import (
-#    network_get,
     config,
     leader_get,
     open_port,
@@ -9,28 +20,16 @@ from charmhelpers.core.hookenv import (
     unit_get,
 )
 
-from charms.reactive import (
-    clear_flag,
-    endpoint_from_flag,
-    is_flag_set,
-    set_flag,
-    when,
-    when_any,
-    when_not,
-)
+from charmhelpers.core.host import service_running, service_start
+
+import charms.apt
 
 from charmhelpers.core import unitdata
 
 import charms.leadership
 
-
-#PRIVATE_IP = network_get('http')['ingress-addresses'][0]
 PRIVATE_IP = unit_get('private-address')
 MAAS_WEB_PORT = 5240
-
-
-set_flag('maas.mode.{}'.format(config('maas-mode')))
-
 
 kv = unitdata.kv()
 
@@ -42,9 +41,14 @@ def maas_url():
         return 'http://{}:5240/MAAS'.format(PRIVATE_IP)
 
 
-@when('postgresql.connected')
-@when_any('maas.mode.region',
-          'maas.mode.region+rack')
+@when_not('layer-maas-region-lxd.installed')
+def install_layer_maas_region_lxd():
+    charms.apt.queue_install(['maas-region-api'])
+    set_flag('layer-maas-region-lxd.installed')
+
+
+@when('postgresql.connected',
+      'apt.installed.maas-region-api')
 @when_not('maas.database.requested')
 def request_postgresql_database_for_maas_region(pgsql):
     """Request PGSql DB
@@ -64,8 +68,7 @@ def request_postgresql_database_for_maas_region(pgsql):
 
 @when('postgresql.master.available',
       'maas.database.requested')
-@when_any('maas.mode.region',
-          'maas.mode.region+rack')
+    #   'apt.installed.maas-region-api')
 @when_not('maas.juju.database.available')
 def get_set_postgresql_data_for_maas_db(pgsql):
     """Get/set postgresql details
@@ -82,62 +85,40 @@ def get_set_postgresql_data_for_maas_db(pgsql):
     set_flag('maas.juju.database.available')
 
 
-@when('snap.installed.maas',
+@when('apt.installed.maas-region-api',
       'leadership.is_leader')
 @when_any('maas.juju.database.available',
           'maas.manual.database.available')
-@when_any('maas.mode.region',
-          'maas.mode.region+rack')
 @when_not('maas.init.complete')
 def maas_leader_init():
     """Init MAAS (region, region+rack) - only leader should run this code
     """
     status_set('maintenance',
-               'Configuring MAAS-{}'.format(config('maas-mode')))
+               'Configuring MAAS-Region')
 
     init_ctxt = {'maas_url': maas_url(),
-                 'maas_mode': config('maas-mode'),
+                #  'maas_mode': config('maas-mode'),
                  'db_host': kv.get('db_host'),
                  'db_name': kv.get('db_name'),
                  'db_pass': kv.get('db_pass'),
                  'db_user': kv.get('db_user')}
-
-    cmd_init = ('maas init --maas-url {maas_url} --database-host {db_host} '
+    cmd_init = ('maas-region local_config_set '
+                '--maas-url {maas_url} --database-host {db_host} '
                 '--database-name {db_name} --database-user {db_user} '
-                '--database-pass {db_pass} --mode {maas_mode} '
-                '--force'.format(**init_ctxt))
-
+                '--database-pass {db_pass} '.format(**init_ctxt))
+    cmd_dbupgrade = ('maas-region dbupgrade'.format(**init_ctxt))
+    # cmd_init = ('maas init --maas-url {maas_url} --database-host {db_host} '
+    #             '--database-name {db_name} --database-user {db_user} '
+    #             '--database-pass {db_pass} --mode {maas_mode} '
+    #             '--force'.format(**init_ctxt))
     call(cmd_init.split())
-    status_set('active', 'MAAS-{} configured'.format(config('maas-mode')))
-    set_flag('maas.init.complete')
-
-
-@when('snap.installed.maas',
-      'leadership.is_leader',
-      'maas.mode.all')
-@when_not('maas.init.complete')
-def maas_init_mode_all():
-    """Init MAAS all mode
-    """
-    status_set('maintenance',
-               'Configuring MAAS-{}'.format(config('maas-mode')))
-
-    init_ctxt = {'maas_url': maas_url(),
-                 'maas_mode': config('maas-mode')}
-
-    cmd_init = ('maas init --maas-url {maas_url} --mode all '
-                '--force --skip-admin'.format(**init_ctxt))
-    call(cmd_init.split())
-
-    status_set('active', 'MAAS-{} configured'.format(config('maas-mode')))
+    call(cmd_dbupgrade.split())
+    status_set('active', 'MAAS-Region configured')
     set_flag('maas.init.complete')
 
 
 @when('leadership.is_leader',
       'maas.init.complete')
-@when_any('maas.mode.region',
-          'maas.mode.region+rack',
-          'maas.mode.all')
 @when_not('maas.admin.created',
           'leadership.set.init_complete')
 def create_maas_admin():
@@ -147,59 +128,64 @@ def create_maas_admin():
                        'admin_username': config('admin-username'),
                        'admin_email': config('admin-email')}
 
-    cmd_create_super_user = ('maas createadmin --username {admin_username} '
-                             '--password {admin_password} '
-                             '--email {admin_email}'.format(**super_user_ctxt))
+    cmd_create_super_user = ('maas init --admin-username {admin_username} '
+                             '--admin-password {admin_password} '
+                             '--admin-email {admin_email}'
+                             .format(**super_user_ctxt))
     call(cmd_create_super_user.split())
-
     # Now the admin is created, init is complete
     charms.leadership.leader_set(init_complete="true")
     set_flag('maas.admin.created')
 
 
+@when('maas.admin.created')
+def start_maas_regiond_service():
+    ''' Start the maas-regiond service. '''
+    status_set('maintenance', 'Starting maas-regiond service.')
+    if service_running('maas-regiond'):
+        set_state('maas-regiond.service.started')
+    else:
+        service_start('maas-regiond')
+    set_state('maas-regiond.service.started')
+
+
 @when('leadership.is_leader',
-      'maas.mode.region',
-      'maas.mode.region+rack',
-      'maas.init.complete')
+      'maas-regiond.service.started')
 @when_not('leadership.set.secret')
 def get_set_secret():
-    with open("/var/snap/maas/current/var/lib/maas/secret", 'r') as f:
+    with open("/var/lib/maas/secret", 'r') as f:
         charms.leadership.leader_set(secret=f.read())
 
 
 @when('leadership.set.secret')
 @when_not('leadership.is_leader',
           'maas.init.complete')
-@when_any('maas.mode.region',
-          'maas.mode.region+rack')
 @when_any('maas.juju.database.available',
           'maas.manual.database.available')
 def init_non_leader_region_or_region_rack():
     status_set('maintenance',
-               'Configuring MAAS-{}'.format(config('maas-mode')))
+               'Configuring MAAS-Region')
 
     init_ctxt = {'maas_url': maas_url(),
-                 'maas_mode': config('maas-mode'),
+                #  'maas_mode': config('maas-mode'),
                  'db_host': kv.get('db_host'),
                  'db_name': kv.get('db_name'),
                  'db_pass': kv.get('db_pass'),
                  'db_user': kv.get('db_user')}
 
-    cmd_init = ('maas config --maas-url {maas_url} --database-host {db_host} '
+    cmd_init = ('maas-region local_config_set '
+                '--maas-url {maas_url} --database-host {db_host} '
                 '--database-name {db_name} --database-user {db_user} '
                 '--database-pass {db_pass} --mode {maas_mode} '
                 '--force'.format(**init_ctxt))
 
     call(cmd_init.split())
 
-    status_set('active', 'MAAS-{} configured'.format(config('maas-mode')))
+    status_set('active', 'MAAS-Region configured')
     set_flag('maas.init.complete')
 
 
 @when('maas.init.complete')
-@when_any('maas.mode.region',
-          'maas.mode.all',
-          'maas.mode.region+rack')
 @when_not('maas.http.available')
 def open_web_port():
     open_port(MAAS_WEB_PORT)
@@ -209,8 +195,6 @@ def open_web_port():
 
 @when('leadership.set.secret',
       'leadership.is_leader',
-      'maas.mode.region',
-      'maas.mode.region+rack',
       'endpoint.region.available')
 @when_not('region.relation.data.available')
 def send_relation_data_to_rack():
@@ -221,61 +205,19 @@ def send_relation_data_to_rack():
     set_flag('region.relation.data.available')
 
 
-@when('maas.mode.rack',
-      'endpoint.rack.available')
-@when_not('rack.relation.data.available')
-def acquire_config_from_region_controller():
-    """Acquire maas_url and secret from region
-    """
-    status_set('maintenance',
-               'Acquiring configuration details from region controller')
-    endpoint = endpoint_from_flag('endpoint.rack.available')
-    services = endpoint.services()
-    for service in services:
-        for host in service['hosts']:
-            kv.set('maas_url', host['maas_url'])
-            kv.set('secret', host['secret'])
-    status_set('active', 'Region configuration acquired')
-    set_flag('rack.relation.data.available')
-
-
-@when('rack.relation.data.available',
-      'maas.mode.rack')
-@when_not('maas.init.complete')
-def configure_maas_rack():
-    """Configure rack controller now that we have what we need
-    """
-    status_set('maintenance', 'Rack initializing')
-    init_ctxt = {'maas_url': kv.get('maas_url'),
-                 'secret': kv.get('secret')}
-    cmd_init = \
-        ('maas init --maas-url {maas_url} --secret {secret} '
-         '--mode rack --force'.format(**init_ctxt))
-    call(cmd_init.split())
-    status_set('active', 'Rack init complete')
-    set_flag('maas.init.complete')
-
-
-@when('maas.init.complete',
-      'maas.mode.rack')
-def set_connected_status():
-    status_set('active', "Region <-> Rack connected")
-
-
-@when('maas.mode.rack')
-@when_not('rack.relation.data.available')
-def block_until_master_relation():
-    """Block rack-controllers until relation to region is acquired
-    """
-    status_set('blocked',
-               'Need relation to region controller to continue')
-    return
+# @when('apt.installed.maas-region-api')
+# @when_not('maas.manual.database.available',
+#           'maas.juju.database.available')
+# def block_until_database_info_set():
+#     """Block region-controller has set database info
+#     """
+#     status_set('blocked',
+#                'Need database connection info via config or relation')
+#     return
 
 
 @when('endpoint.http.joined')
-@when_any('maas.mode.region',
-          'maas.mode.all',
-          'maas.mode.region+rack')
+    #   'apt.installed.maas-region-api')
 @when_not('http.relation.data.available')
 def set_http_relation_data():
     endpoint = endpoint_from_flag('endpoint.http.joined')
